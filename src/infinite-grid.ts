e08e68b1156403f5a8e37b0e328f3473e08e8c94
leapar <leapar@163.com>
2024-12-15 10:39:05 +0800

    Initial commit

---
+++ src/infinite-grid.ts
@@ -0,0 +1,70 @@
+import {
+    BLENDMODE_ONE,
+    BLENDMODE_ONE_MINUS_SRC_ALPHA,
+    BLENDMODE_SRC_ALPHA,
+    BLENDEQUATION_ADD,
+    CULLFACE_NONE,
+    FUNC_LESSEQUAL,
+    SEMANTIC_POSITION,
+    createShaderFromCode,
+    BlendState,
+    DepthState,
+    Layer,
+    QuadRender,
+    Shader
+} from 'playcanvas';
+
+import { Element, ElementType } from './element';
+import { Serializer } from './serializer';
+import { vertexShader, fragmentShader } from './shaders/infinite-grid-shader';
+
+class InfiniteGrid extends Element {
+    shader: Shader;
+    quadRender: QuadRender;
+    blendState = new BlendState(false);
+    depthState = new DepthState(FUNC_LESSEQUAL, true);
+
+    visible = true;
+
+    constructor() {
+        super(ElementType.debug);
+    }
+
+    add() {
+        const device = this.scene.app.graphicsDevice;
+
+        this.shader = createShaderFromCode(device, vertexShader, fragmentShader, 'infinite-grid', {
+            vertex_position: SEMANTIC_POSITION
+        });
+
+        this.quadRender = new QuadRender(this.shader);
+
+        const blendState = new BlendState(
+            true,
+            BLENDEQUATION_ADD, BLENDMODE_SRC_ALPHA, BLENDMODE_ONE_MINUS_SRC_ALPHA,
+            BLENDEQUATION_ADD, BLENDMODE_ONE, BLENDMODE_ONE_MINUS_SRC_ALPHA
+        );
+
+        this.scene.camera.entity.camera.on('preRenderLayer', (layer: Layer, transparent: boolean) => {
+            if (this.visible && layer === this.scene.debugLayer && !transparent) {
+                device.setBlendState(blendState);
+                device.setCullMode(CULLFACE_NONE);
+                device.setDepthState(DepthState.WRITEDEPTH);
+                device.setStencilState(null, null);
+
+                this.quadRender.render();
+            }
+        });
+    }
+
+    remove() {
+        this.shader.destroy();
+        this.quadRender.destroy();
+    }
+
+    serialize(serializer: Serializer): void {
+        serializer.pack(this.visible);
+    }
+}
+
+export { InfiniteGrid };
---
+++ src/outline.ts
@@ -0,0 +1,100 @@
+import {
+    CULLFACE_NONE,
+    SEMANTIC_POSITION,
+    createShaderFromCode,
+    BlendState,
+    DepthState,
+    Color,
+    Entity,
+    Layer,
+    Shader,
+    QuadRender,
+    WebglGraphicsDevice
+} from 'playcanvas';
+
+import { Element, ElementType } from './element';
+import { vertexShader, fragmentShader } from './shaders/outline-shader';
+
+class Outline extends Element {
+    entity: Entity;
+    shader: Shader;
+    quadRender: QuadRender;
+    enabled = true;
+    clr = new Color(1, 1, 1, 0.5);
+
+    constructor() {
+        super(ElementType.other);
+
+        this.entity = new Entity('outlineCamera');
+        this.entity.addComponent('camera');
+        this.entity.camera.setShaderPass('OUTLINE');
+        this.entity.camera.clearColor = new Color(0, 0, 0, 0);
+    }
+
+    add() {
+        const device = this.scene.app.graphicsDevice;
+
+        // render overlay layer only
+        this.entity.camera.layers = [this.scene.overlayLayer.id];
+        this.scene.camera.entity.addChild(this.entity);
+
+        this.shader = createShaderFromCode(device, vertexShader, fragmentShader, 'apply-outline', {
+            vertex_position: SEMANTIC_POSITION
+        });
+
+        this.quadRender = new QuadRender(this.shader);
+
+        const outlineTextureId = device.scope.resolve('outlineTexture');
+        const clrId = device.scope.resolve('clr');
+        const clr = this.clr;
+        const clrStorage = [1, 1, 1, 1];
+
+        // apply the outline texture to the display before gizmos render
+        this.entity.camera.on('postRenderLayer', (layer: Layer, transparent: boolean) => {
+            if (!this.entity.enabled || layer !== this.scene.overlayLayer || !transparent) {
+                return;
+            }
+
+            device.setBlendState(BlendState.ALPHABLEND);
+            device.setCullMode(CULLFACE_NONE);
+            device.setDepthState(DepthState.NODEPTH);
+            device.setStencilState(null, null);
+
+            clrStorage[0] = clr.r;
+            clrStorage[1] = clr.g;
+            clrStorage[2] = clr.b;
+            clrStorage[3] = clr.a;
+
+            outlineTextureId.setValue(this.entity.camera.renderTarget.colorBuffer);
+            clrId.setValue(clrStorage);
+
+            const glDevice = device as WebglGraphicsDevice;
+            glDevice.setRenderTarget(this.scene.camera.entity.camera.renderTarget);
+            glDevice.updateBegin();
+            this.quadRender.render();
+            glDevice.updateEnd();
+        });
+    }
+
+    remove() {
+        this.scene.camera.entity.removeChild(this.entity);
+    }
+
+    onPreRender() {
+        // copy camera properties
+        const src = this.scene.camera.entity.camera;
+        const dst = this.entity.camera;
+
+        dst.projection = src.projection;
+        dst.horizontalFov = src.horizontalFov;
+        dst.fov = src.fov;
+        dst.nearClip = src.nearClip;
+        dst.farClip = src.farClip;
+        dst.orthoHeight = src.orthoHeight;
+
+        this.entity.enabled = this.enabled && this.scene.events.invoke('view.outlineSelection');
+        this.entity.camera.renderTarget = this.scene.camera.workRenderTarget;
+    }
+}
+
+export { Outline };
